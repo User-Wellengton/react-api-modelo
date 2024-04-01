0b1dec9f770121058c9c2ca5995b55dc30c342dc
Wellengton <wellengton.machado@wansoft.com.br>
2024-04-01 12:55:05 -0300

    Inserido Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import ProdutoListar from "./components/Produto/ProdutoListar";
+import Navbar from "./components/NavBar/Navbar";
 
 const App: React.FC = function () {
   return (
     <Router>
+      <Navbar />
       <Routes>
         <Route path="/" element={<h1>Seja Bem Vindo!</h1>} />
         <Route path="/Produto" element={<ProdutoListar />} />
---
+++ src/components/NavBar/Navbar.tsx
@@ -0,0 +1,24 @@
+const Navbar: React.FC = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div className="container-fluid">
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="/">
+                Home
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/Produto">
+                Produto
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
